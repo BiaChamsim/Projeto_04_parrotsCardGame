bd8016be734ccb3a5feee762a70a53844a585aea
Beatriz Chamsim <biachamsim@gmail.com>
2022-04-06 18:26:11 -0300

    script prompt inicial

--- javascript.js
+++ javascript.js
@@ -1,2 +1,7 @@
-let cartas = [];
-cartas.push(prompt("Com quantas cartas entre 4 e 14 você quer jogar?"))
+let qtdCartas = Number(prompt("Com quantas cartas entre 4 e 14 você quer jogar?"));
+
+while( (qtdCartas % 2) === 1 || qtdCartas < 4 || qtdCartas > 14) {
+    qtdCartas = Number(prompt("Com quantas cartas entre 4 e 14 você quer jogar?"));
+}
+
+console.log(qtdCartas)
